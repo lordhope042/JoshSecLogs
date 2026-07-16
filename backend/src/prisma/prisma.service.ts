fc30587b62179b04616de883f30f3ca53a07e554
lordhope042 <07033087993hope@gmail.com>
2026-07-16 16:20:02 +0100

    refactor: simplify Prisma configuration

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,24 +1,19 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-
   constructor() {
-    const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    });
-
-    super({
-      adapter,
-    });
+    super();
   }
 
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async enableShutdownHooks() {
+    process.on('beforeExit', async () => {
+      await this.$disconnect();
+    });
   }
 }
