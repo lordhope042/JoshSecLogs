701fcbf6d796d25276e4201c6c2522e5dcbd6df3
lordhope042 <07033087993hope@gmail.com>
2026-07-16 14:48:27 +0100

    fix supabase prisma ssl connection

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -6,7 +6,6 @@ import { PrismaPg } from '@prisma/adapter-pg';
 export class PrismaService extends PrismaClient implements OnModuleInit {
 
   constructor() {
-
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
       ssl: {
